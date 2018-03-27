bd1e20d0eb9e42b12aadaed4af07477a4f2d6457
Michael Joyce <michael.joyce@celtechgroup.com>
2018-03-27 15:32:01 +0100

    Working on tutorial of react application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>Hi, I am react app</h1>
-      </div>
-    );
-  }
+    state = {
+        persons: [
+            {name: 'Max', age: 12},
+            {name: 'Michael', age: 16},
+            {name: 'Sean', age: 19},
+        ],
+        showPersons: false
+    };
+    nameChangedHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({
+            showPersons: !doesShow
+        })
+    };
+
+    deletePersonHandler = (index) => {
+        const persons = this.state.persons;
+        persons.splice(index, 1);
+        this.setState({
+            persons: persons
+        })
+    };
+
+    render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
+        let persons = null;
+        if(this.state.showPersons){
+            persons = (
+                <div>
+                    {this.state.persons.map((person, index) => {
+                        return (
+                            <Person
+                                name={person.name}
+                                age={person.age}
+                                click={() => this.deletePersonHandler(index)}>
+                            </Person>
+                        )
+                    })}
+                </div>
+            );
+        }
+        return (
+            <div className="App">
+                <h1>Hi, I am react app</h1>
+                <p>This is really working</p>
+                <button style={style} onClick={this.nameChangedHandler}>Toggle People</button>
+                {persons}
+            </div>
+        );
+    }
 }
 
 export default App;
